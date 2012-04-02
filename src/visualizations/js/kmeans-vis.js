924e6ab78678864496aeb5e609fd9ed5148134f2
Vassilis Triglianos <triglian@gmail.com>
2012-04-03 01:51:09 +0300

    Animate centroids

--- src/visualizations/js/kmeans-vis.js
+++ src/visualizations/js/kmeans-vis.js
@@ -1,4 +1,7 @@
-var initialCentroids = [[3,3], [6, 2], [8, 5]];
+var initialCentroids = [[3,3], [6, 2], [8, 5]],
+centroidPaths = [new Path(), new Path(), new Path()],
+centroidColors = ['#ff000', '#00ff00', '#0000ff']
+centroidMoves = [];
 /*Find Range */
 var minMax = findMinMax(inputData),
 cW = 600,
@@ -26,10 +29,11 @@ function drawDataPoints(){
 function drawCentroids(centroids){
     for (var i=0; i<centroids.length; i++){
         var myPoint = new Point(centroids[i][0]*norX, cH - (centroids[i][1] * norY));
-        var myPath = new Path.Circle(myPoint, 5);
-        myPath.strokeColor = '#8c6239';
-        myPath.fillColor = '#ed145b';
-        myPath.strokeWidth = 2;
+        centroidPaths[i].removeSegments();
+        centroidPaths[i] = new Path.Circle(myPoint, 5);
+        centroidPaths[i].strokeColor = '#8c6239';
+        centroidPaths[i].fillColor = centroidColors[i];
+        centroidPaths[i].strokeWidth = 2;
         
     }
 }
@@ -49,6 +53,45 @@ function findMinMax(data){
     return {'minx':minx, 'miny': miny, 'maxx': maxx, 'maxy': maxy}
 }
 
-drawDataPoints()
-drawCentroids(initialCentroids)
+drawDataPoints();
+drawCentroids(initialCentroids);
+
+var kmeans = new Kmeans(inputData, initialCentroids, 100, function(cpoints){
+    centroidMoves.push(cpoints)
+})
+
+kmeans.run();
+
+
+var l = centroidMoves.length;
+var cnt = 0
+animateCentroids = true
+
+
+function onFrame(event) {
+    if(animateCentroids){
+    
+    //drawCentroids(centroidMoves[cnt]);
+    cnt =  Math.floor(event.time / 0.1)
+     if(cnt < l){   
+        drawCentroids(centroidMoves[cnt]);
+            
+    }else{
+        animateCentroids = false
+    }
+    //console.log (cnt)
+/*     animateCentroids = false */
+    // the number of times the frame event was fired:
+/*     console.log(event.count); */
+
+    // The total amount of time passed since
+    // the first frame event in seconds:
+/*     console.log(event.time); */
+
+    // The time passed in seconds since the last frame event:
+/*     console.log(event.delta); */
+
+    
+    }
+}
 
